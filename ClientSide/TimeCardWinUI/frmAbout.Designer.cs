6d015eb34b0a7288d8ba10b8fd54a996d18f0109
patnir <rahul20patni@gmail.com>
2016-06-08 13:37:12 -0400

    Change to more descrptive labels in forms

--- ClientSide/TimeCardWinUI/frmAbout.Designer.cs
+++ ClientSide/TimeCardWinUI/frmAbout.Designer.cs
@@ -5,87 +5,87 @@
 
     private void InitializeComponent()
     {
-        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAbout));
-        this.picIcon = new System.Windows.Forms.PictureBox();
-        this.lblCopyright = new System.Windows.Forms.Label();
-        this.lblAppTitle = new System.Windows.Forms.Label();
-        this.cmdOK = new System.Windows.Forms.Button();
-        this.label1 = new System.Windows.Forms.Label();
-        ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
-        this.SuspendLayout();
-        // 
-        // picIcon
-        // 
-        this.picIcon.Image = ((System.Drawing.Image)(resources.GetObject("picIcon.Image")));
-        this.picIcon.Location = new System.Drawing.Point(14, 18);
-        this.picIcon.Name = "picIcon";
-        this.picIcon.Size = new System.Drawing.Size(32, 32);
-        this.picIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-        this.picIcon.TabIndex = 15;
-        this.picIcon.TabStop = false;
-        // 
-        // lblCopyright
-        // 
-        this.lblCopyright.AutoSize = true;
-        this.lblCopyright.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        this.lblCopyright.Location = new System.Drawing.Point(12, 112);
-        this.lblCopyright.Name = "lblCopyright";
-        this.lblCopyright.Size = new System.Drawing.Size(334, 13);
-        this.lblCopyright.TabIndex = 14;
-        this.lblCopyright.Text = "Copyright (c) 2011 Department of Computer && Information Technology";
-        this.lblCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        // 
-        // lblAppTitle
-        // 
-        this.lblAppTitle.AutoSize = true;
-        this.lblAppTitle.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        this.lblAppTitle.Location = new System.Drawing.Point(58, 18);
-        this.lblAppTitle.Name = "lblAppTitle";
-        this.lblAppTitle.Size = new System.Drawing.Size(282, 40);
-        this.lblAppTitle.TabIndex = 13;
-        this.lblAppTitle.Text = "CIT 450 TimeCard";
-        this.lblAppTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        // 
-        // cmdOK
-        // 
-        this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-        this.cmdOK.Location = new System.Drawing.Point(279, 147);
-        this.cmdOK.Name = "cmdOK";
-        this.cmdOK.Size = new System.Drawing.Size(75, 23);
-        this.cmdOK.TabIndex = 12;
-        this.cmdOK.Text = "&OK";
-        this.cmdOK.Click += new System.EventHandler(this.btnOK_Click);
-        // 
-        // label1
-        // 
-        this.label1.AutoSize = true;
-        this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        this.label1.Location = new System.Drawing.Point(12, 92);
-        this.label1.Name = "label1";
-        this.label1.Size = new System.Drawing.Size(57, 13);
-        this.label1.TabIndex = 16;
-        this.label1.Text = "Vesion 1.0";
-        this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-        // 
-        // frmAbout
-        // 
-        this.AcceptButton = this.cmdOK;
-        this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-        this.CancelButton = this.cmdOK;
-        this.ClientSize = new System.Drawing.Size(366, 182);
-        this.Controls.Add(this.label1);
-        this.Controls.Add(this.picIcon);
-        this.Controls.Add(this.lblCopyright);
-        this.Controls.Add(this.lblAppTitle);
-        this.Controls.Add(this.cmdOK);
-        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-        this.MaximizeBox = false;
-        this.Name = "frmAbout";
-        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-        this.Tag = "Fall2011";
-        ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
-        this.ResumeLayout(false);
-        this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAbout));
+            this.picIcon = new System.Windows.Forms.PictureBox();
+            this.lblCopyright = new System.Windows.Forms.Label();
+            this.lblAppTitle = new System.Windows.Forms.Label();
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picIcon
+            // 
+            this.picIcon.Image = ((System.Drawing.Image)(resources.GetObject("picIcon.Image")));
+            this.picIcon.Location = new System.Drawing.Point(14, 18);
+            this.picIcon.Name = "picIcon";
+            this.picIcon.Size = new System.Drawing.Size(32, 32);
+            this.picIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picIcon.TabIndex = 15;
+            this.picIcon.TabStop = false;
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.AutoSize = true;
+            this.lblCopyright.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyright.Location = new System.Drawing.Point(12, 112);
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(62, 13);
+            this.lblCopyright.TabIndex = 14;
+            this.lblCopyright.Text = "Rahul Patni";
+            this.lblCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAppTitle
+            // 
+            this.lblAppTitle.AutoSize = true;
+            this.lblAppTitle.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppTitle.Location = new System.Drawing.Point(58, 18);
+            this.lblAppTitle.Name = "lblAppTitle";
+            this.lblAppTitle.Size = new System.Drawing.Size(157, 40);
+            this.lblAppTitle.TabIndex = 13;
+            this.lblAppTitle.Text = "TimeCard";
+            this.lblAppTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdOK.Location = new System.Drawing.Point(279, 147);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(75, 23);
+            this.cmdOK.TabIndex = 12;
+            this.cmdOK.Text = "&OK";
+            this.cmdOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Vesion 1.0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmAbout
+            // 
+            this.AcceptButton = this.cmdOK;
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.CancelButton = this.cmdOK;
+            this.ClientSize = new System.Drawing.Size(366, 182);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picIcon);
+            this.Controls.Add(this.lblCopyright);
+            this.Controls.Add(this.lblAppTitle);
+            this.Controls.Add(this.cmdOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmAbout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Tag = "Fall2011";
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
